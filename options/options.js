5648a4b3840348f6d3b3fe27202a232126e659b1
Prosony <Prosony@bk.ru>
2017-10-22 15:45:16 +0300

    0.0.2

---
+++ options/options.js
@@ -0,0 +1,9 @@
+var options = {
+    path: "http://185.77.205.82:3000",
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer token"
+    }
+};
+module.exports.options = options;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var request = require('request');
+// var options = require('./options/options.js');
 
 var app = express();
 var port = 3000;
@@ -9,7 +11,60 @@ var id_account;
 var token;
 app.use(express.static(path.join(__dirname, 'view')));
 
-app.get('/', function(request, responce){
+app.get('/sign-in',function(request, responce){
+    responce.sendFile(path.join(__dirname,'/view/sign-in.html'));
+});
+
+app.post('/check', function(request, responce){
+    // console.log('request',request);
+    request.on('data', function (data) {
+        var answer = JSON.parse(data);
+        id_account = answer.id;
+        token = answer.token;
+        var opt = {
+            hostname: 'localhost',
+            port: 8080,
+            path: '/profile',
+            method: 'POST',
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+        };
+        const req = http.request(opt, (res) => {
+            console.log(`STATUS: ${res.statusCode}`);
+            console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+              console.log(`BODY: ${chunk}`);
+            });
+            res.on('end', () => {
+              console.log('No more data in response.');
+            });
+          });
+          
+          req.on('error', (e) => {
+            console.error(`problem with request: ${e.message}`);
+          });
+          
+          // write data to request body
+          req.write(JSON.stringify({'token':token}));
+          req.end();
+    });
+        // console.log('#INFO address: %s',request.url);
+        // console.log('id_account %s, token: %s',id_account,token);
+
+        // if(id_account.trim() && token.trim()){
+        //     console.log('token and id not empty');
+        //     // responce.redirect(200,'/profile');
+        //     responce.send({err: 0, redirectUrl: "/profile"});
+        //     // responce.send({redirect: '/profile'});
+        // }else{
+        //     responce.send({err: 204, redirectUrl: "/sign-in"});
+        // }
+});
+
+app.route('/profile')
+.get(function(request, responce){
     console.log('#INFO address: %s',request.url);
     
     if(id_account !=undefined && token !=undefined ){
@@ -22,31 +77,10 @@ app.get('/', function(request, responce){
         responce.clearCookie('anotherCookie');
         responce.sendFile(path.join(__dirname,'/view/profile.html'));    
     }else{
-        responce.sendFile(path.join(__dirname,'/view/sign-in.html'));
+        responce.redirect('/sign-in');
+        // responce.sendFile(path.join(__dirname,'/view/sign-in.html'));
     }
 });
-
-app.get('/sign-in',function(request, responce){
-    responce.sendFile(path.join(__dirname,'/view/sign-in.html'));
-});
-
-app.post('/check', function(request, responce){
-    // console.log('request',request);
-    request.on('data', function (data) {
-        var answer = JSON.parse(data);
-        id_account = answer.id;
-        token = answer.token;
-        
-        console.log('#INFO address: %s',request.url);
-        console.log('id_account %s, token: %s',id_account,token);
-
-        if(id_account.trim() && token.trim()){
-            console.log('token and id not empty');
-        }
-
-    });
-});
-
 app.listen(port, function(){
     console.log('server start on port 3000');
 });
